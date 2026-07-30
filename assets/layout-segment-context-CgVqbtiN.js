2a6b3dc766e28ff0bd67f52403c756931bbe5822
tokicx <tokicx@gmail.com>
2026-07-30 10:05:09 +0200

    Publish KindMoment Aurora web
    Deploy the reviewed Aurora design while preserving the existing Apple and Google legal/support URLs.

---
+++ assets/layout-segment-context-CgVqbtiN.js
@@ -0,0 +1,1 @@
+import{r as e}from"./rolldown-runtime-QTnfLwEv.js";import{i as t}from"./framework-B5gSXtEp.js";import{t as n}from"./index-Khk-bTNC.js";var r=e(t(),1);function i({segmentMap:e,children:t}){let i=n();return i?(0,r.createElement)(i.Provider,{value:e},t):t}export{i as LayoutSegmentProvider};
